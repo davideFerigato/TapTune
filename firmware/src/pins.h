aadabce59a317ca02bb4de8554c7b8a68b69ce7a
Davide Maria Ferigato <d.m.f.s@outlook.it>
2026-02-23 00:50:18 +0100

    add

---
+++ firmware/src/pins.h
@@ -0,0 +1,19 @@
+#ifndef PINS_H
+#define PINS_H
+
+// LED RGB
+#define PIN_LED_R   32
+#define PIN_LED_G   33
+#define PIN_LED_B   27
+
+// Buzzer
+#define PIN_BUZZER  14
+
+// Audio PWM
+#define PIN_AUDIO_PWM 5
+
+// I2C per display
+#define PIN_I2C_SDA 21
+#define PIN_I2C_SCL 22
+
+#endif
